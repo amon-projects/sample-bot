b41f0edb7c614f90690a48742f4585b7e8c5747a
iqpizza6349 <iqpizza6349@gmail.com>
2023-10-01 23:23:53 +0900

    initialize and create sample bot

---
+++ README.md
@@ -0,0 +1,4 @@
+# SampleBot
+
+This project is exact same as [davechurchill's STARTcraft](https://github.com/davechurchill/STARTcraft)
+but not fork.
---
+++ src/BotModule/BotModule.cpp
@@ -0,0 +1,169 @@
+﻿#include "BotModule.h"
+#include "Tools.h"
+#include "MapTools.h"
+
+BotModule::BotModule()
+{
+    
+}
+
+// called when the bot starts!
+void BotModule::onStart()
+{
+    // 스타크래프트는 24 프레임 게임이니, 정상적인 속도를 원한다면 24를
+    // 제일 빠르게 하고 싶다면 0으로 설정하면 된다.
+    BWAPI::Broodwar->setLocalSpeed(10);
+    BWAPI::Broodwar->setFrameSkip(0);
+
+    BWAPI::Broodwar->enableFlag(BWAPI::Flag::UserInput);
+    
+    m_mapTools.onStart();
+}
+
+// called on each frame of the game
+void BotModule::onFrame()
+{
+    m_mapTools.onFrame();
+
+    // send our idle workers to mine minerals so they don't just stand there
+    sendIdleWorkersToMinerals();
+
+    // train more workers so we can gather more income
+    trainAdditionalWorkers();
+
+    // build more supply if we are going to run out soon
+    buildAdditionalSupply();
+
+    // draw unit health bars, which brood war unfortunately does not do
+    Tools::DrawUnitHealthBars();
+
+    // draw some relevent information to the screen to help us debug the bot
+    drawDebugInformation();
+}
+
+// Send our idle workers to mine minerals so they don't just stand there
+void BotModule::sendIdleWorkersToMinerals()
+{
+    // Let's send all of our starting workers to the closest mineral to them
+    // First we need to loop over all of the units that we (BWAPI::Broodwar->self()) own
+    const BWAPI::Unitset& myUnits = BWAPI::Broodwar->self()->getUnits();
+    for (auto& unit : myUnits)
+    {
+        // Check the unit type, if it is an idle worker, then we want to send it somewhere
+        if (unit->getType().isWorker() && unit->isIdle())
+        {
+            // Get the closest mineral to this worker unit
+            BWAPI::Unit closestMineral = Tools::GetClosestUnitTo(unit, BWAPI::Broodwar->getMinerals());
+
+            // If a valid mineral was found, right click it with the unit in order to start harvesting
+            if (closestMineral) { unit->rightClick(closestMineral); }
+        }
+    }
+}
+
+// Train more workers so we can gather more income
+void BotModule::trainAdditionalWorkers()
+{
+    const BWAPI::UnitType workerType = BWAPI::Broodwar->self()->getRace().getWorker();
+    const int workersWanted = 20;
+    const int workersOwned = Tools::CountUnitsOfType(workerType, BWAPI::Broodwar->self()->getUnits());
+    if (workersOwned < workersWanted)
+    {
+        // get the unit pointer to my depot
+        const BWAPI::Unit myDepot = Tools::GetDepot();
+
+        // if we have a valid depot unit and it's currently not training something, train a worker
+        // there is no reason for a bot to ever use the unit queueing system, it just wastes resources
+        if (myDepot && !myDepot->isTraining()) { myDepot->train(workerType); }
+    }
+}
+
+// Build more supply if we are going to run out soon
+void BotModule::buildAdditionalSupply()
+{
+    // Get the amount of supply supply we currently have unused
+    const int unusedSupply = Tools::GetTotalSupply(true) - BWAPI::Broodwar->self()->supplyUsed();
+
+    // If we have a sufficient amount of supply, we don't need to do anything
+    if (unusedSupply >= 2) { return; }
+
+    // Otherwise, we are going to build a supply provider
+    const BWAPI::UnitType supplyProviderType = BWAPI::Broodwar->self()->getRace().getSupplyProvider();
+
+    const bool startedBuilding = Tools::BuildBuilding(supplyProviderType);
+    if (startedBuilding)
+    {
+        BWAPI::Broodwar->printf("Started Building %s", supplyProviderType.getName().c_str());
+    }
+}
+
+// Draw some relevent information to the screen to help us debug the bot
+void BotModule::drawDebugInformation()
+{
+    BWAPI::Broodwar->drawTextScreen(BWAPI::Position(10, 10), "Hello, World!\n");
+    Tools::DrawUnitCommands();
+    Tools::DrawUnitBoundingBoxes();
+}
+
+// Called whenever the game ends and tells you if you won or not
+void BotModule::onEnd(bool isWinner)
+{
+    std::cout << "We " << (isWinner ? "won!" : "lost!") << "\n";
+}
+
+// Called whenever a unit is destroyed, with a pointer to the unit
+void BotModule::onUnitDestroy(BWAPI::Unit unit)
+{
+
+}
+
+// Called whenever a unit is morphed, with a pointer to the unit
+// Zerg units morph when they turn into other units
+void BotModule::onUnitMorph(BWAPI::Unit unit)
+{
+
+}
+
+// Called whenever a text is sent to the game by a user
+void BotModule::onSendText(std::string text)
+{
+    if (text == "/map")
+    {
+        m_mapTools.toggleDraw();
+    }
+}
+
+// Called whenever a unit is created, with a pointer to the destroyed unit
+// Units are created in buildings like barracks before they are visible, 
+// so this will trigger when you issue the build command for most units
+void BotModule::onUnitCreate(BWAPI::Unit unit)
+{
+
+}
+
+// Called whenever a unit finished construction, with a pointer to the unit
+void BotModule::onUnitComplete(BWAPI::Unit unit)
+{
+
+}
+
+// Called whenever a unit appears, with a pointer to the destroyed unit
+// This is usually triggered when units appear from fog of war and become visible
+void BotModule::onUnitShow(BWAPI::Unit unit)
+{
+
+}
+
+// Called whenever a unit gets hidden, with a pointer to the destroyed unit
+// This is usually triggered when units enter the fog of war and are no longer visible
+void BotModule::onUnitHide(BWAPI::Unit unit)
+{
+
+}
+
+// Called whenever a unit switches player control
+// This usually happens when a dark archon takes control of a unit
+void BotModule::onUnitRenegade(BWAPI::Unit unit)
+{
+
+}
